d68a01510f01fdad002d9f98a8ff26e68317d54f
Mark Slater <mark.slater@mail.com>
2017-04-27 14:47:30 +0100

    Shabby code to pick up activity start events.

--- src/main/java/LoxoneHarmonyIntegration.java
+++ src/main/java/LoxoneHarmonyIntegration.java
@@ -1,10 +1,14 @@
+import argo.jdom.JdomParser;
+import argo.jdom.JsonNode;
+import argo.saj.InvalidSyntaxException;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.StanzaCollector;
+import org.jivesoftware.smack.StanzaListener;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.filter.StanzaFilter;
 import org.jivesoftware.smack.packet.*;
 import org.jivesoftware.smack.provider.IQProvider;
 import org.jivesoftware.smack.provider.ProviderManager;
-import org.jivesoftware.smack.sm.predicates.ForEveryStanza;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jivesoftware.smack.util.PacketParserUtils;
@@ -13,8 +17,17 @@ import org.jxmpp.jid.impl.JidCreate;
 import org.jxmpp.jid.parts.Resourcepart;
 import org.xmlpull.v1.XmlPullParser;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+import java.io.StringReader;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.xmlpull.v1.XmlPullParser.END_TAG;
 
 public final class LoxoneHarmonyIntegration {
@@ -72,8 +85,8 @@ public final class LoxoneHarmonyIntegration {
             }
         };
 
-        authConnection.addPacketSendingListener(stanza -> System.out.println(stanza.toXML().toString()), ForEveryStanza.INSTANCE);
-        authConnection.addSyncStanzaListener(stanza -> System.out.println(stanza.toXML().toString()), ForEveryStanza.INSTANCE);
+//        authConnection.addPacketSendingListener(stanza -> System.out.println(stanza.toXML().toString()), ForEveryStanza.INSTANCE);
+//        authConnection.addSyncStanzaListener(stanza -> System.out.println(stanza.toXML().toString()), ForEveryStanza.INSTANCE);
 
         authConnection.connect();
         authConnection.login("guest@connect.logitech.com/gatorade.", "gatorade.", Resourcepart.from("auth"));
@@ -99,8 +112,131 @@ public final class LoxoneHarmonyIntegration {
         // all the other jibber jabber can go straight in the bin.
         // once we've got username and password we make *another* connection using those rather than the stuff we had before.
         final OaIdentity oaIdentity = OaIdentity.parseHarmonyHubResponse(((UnparsedIQ) stanza).getContent().toString());
-        System.out.println("oaIdentity.asString() = " + oaIdentity.asString());
         authConnection.disconnect();
 
+        final XMPPTCPConnection mainConnection = new XMPPTCPConnection(xmpptcpConnectionConfiguration) {
+            @Override
+            protected void parseAndProcessStanza(XmlPullParser parser) throws Exception {
+                ParserUtils.assertAtStartTag(parser);
+                int parserDepth = parser.getDepth();
+                Stanza stanza = null;
+                try {
+                    if (IQ.IQ_ELEMENT.equals(parser.getName()) && parser.getAttributeValue("", "type") == null) {
+                        // Acknowledgement IQs don't contain a type so an empty result is created here to prevent a parsing NPE
+                        stanza = new EmptyResultIQ();
+                    } else {
+                        stanza = PacketParserUtils.parseStanza(parser);
+                    }
+                } catch (Exception e) {
+                    CharSequence content = PacketParserUtils.parseContentDepth(parser, parserDepth);
+                    System.err.println(e.getMessage());
+                }
+                ParserUtils.assertAtEndTag(parser);
+                if (stanza != null) {
+                    processStanza(stanza);
+                }
+            }
+
+            @Override
+            public void sendStanza(Stanza stanza) throws SmackException.NotConnectedException, InterruptedException {
+                if (stanza.getError() == null || stanza.getError().getCondition() != XMPPError.Condition.service_unavailable) {
+                    super.sendStanza(stanza);
+                }
+            }
+        };
+
+//        mainConnection.addPacketSendingListener(s -> System.out.println(s.toXML().toString()), ForEveryStanza.INSTANCE);
+//        mainConnection.addSyncStanzaListener(s -> System.out.println(s.toXML().toString()), ForEveryStanza.INSTANCE);
+
+        mainConnection.connect();
+        mainConnection.login(oaIdentity.asString() + "@connect.logitech.com/gatorade", oaIdentity.asString(), Resourcepart.from("main"));
+        mainConnection.setFromMode(XMPPConnection.FromMode.USER);
+
+        mainConnection.addSyncStanzaListener(new StanzaListener() {
+            @Override
+            public void processStanza(Stanza stanza) throws SmackException.NotConnectedException {
+                System.out.println("On");
+            }
+        }, new StanzaFilter() {
+            @Override
+            public boolean accept(Stanza stanza) {
+                ExtensionElement event = stanza.getExtension("event", "connect.logitech.com");
+                if (event == null) {
+                    return false;
+                }
+                final String type = ((StandardExtensionElement) event).getAttributeValue("type");
+                if (!"connect.stateDigest?notify".equals(type)) {
+                    return false;
+                }
+
+                final String text = ((StandardExtensionElement) event).getText();
+                final JsonNode jsonNode;
+                try {
+                    jsonNode = new JdomParser().parse(text);
+                } catch (InvalidSyntaxException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+
+                return
+                        jsonNode.isNumberValue("activityStatus") && "1".equals(jsonNode.getNumberValue("activityStatus"))
+//                        && jsonNode.isNumberValue("activityId") && "23648476".equals("activityId"); // fire tv
+                                && jsonNode.isStringValue("activityId") && "23649686".equals(jsonNode.getStringValue("activityId")); // living room sonos
+            }
+        });
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mainConnection.sendStanza(new IQ(new SimpleIQ("oa", "connect.logitech.com") {
+                    }) {
+                        @Override
+                        protected IQChildElementXmlStringBuilder getIQChildElementBuilder(IQChildElementXmlStringBuilder xml) {
+                            final String mimeType = "vnd.logitech.connect/vnd.logitech.ping";
+                            xml.attribute("mime", mimeType);
+                            xml.rightAngleBracket();
+                            return xml;
+                        }
+                    });
+                } catch (SmackException.NotConnectedException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 30, 30, SECONDS);
+
+
+        Thread.sleep(100000);
+        scheduledExecutorService.shutdown();
+        mainConnection.disconnect();
     }
+
+    private static String extractResponseBody(String harmonyHubEvent) throws OaIdentity.OaIdentityParseException {
+        Optional<String> result = Optional.empty();
+        final XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        try {
+            final XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new StringReader(harmonyHubEvent));
+            try {
+                while (xmlStreamReader.hasNext()) {
+                    final int elementType = xmlStreamReader.next();
+
+                    if (
+                            XMLEvent.START_ELEMENT == elementType
+                                    && "event".equals(xmlStreamReader.getLocalName())
+                                    && "connect.stateDigest?notify".equals(xmlStreamReader.getAttributeValue(null, "type"))
+                            ) {
+                        final String elementText = xmlStreamReader.getElementText();
+                        result = Optional.of(elementText);
+                    }
+                }
+            } finally {
+                xmlStreamReader.close();
+            }
+        } catch (XMLStreamException e) {
+            throw new OaIdentity.OaIdentityParseException("Harmony Hub response is not valid XML", e);
+        }
+        return result.orElseThrow(() -> new OaIdentity.OaIdentityParseException("Harmony Hub response has no content"));
+    }
+
 }
