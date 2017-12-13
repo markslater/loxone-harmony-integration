0b2c56c2e2b6955a5803e7f59a1169940f650a12
Mark Slater <mark.slater@mail.com>
2017-12-13 22:59:19 +0000

    Deploy as a service on install.

---
+++ deploy/postInstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+systemctl enable loxone-harmony-integration.service
+systemctl start loxone-harmony-integration.service
---
+++ deploy/preUninstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+systemctl stop loxone-harmony-integration.service
+systemctl disable loxone-harmony-integration.service
