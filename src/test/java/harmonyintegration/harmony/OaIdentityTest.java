eb70fc0d9dcee03194b1d1170dd10dd75bd86df3
Mark Slater <mark.slater@mail.com>
2020-09-01 21:51:39 +0100

    Drivebys.

--- src/test/java/harmonyintegration/harmony/OaIdentityTest.java
+++ src/test/java/harmonyintegration/harmony/OaIdentityTest.java
@@ -9,25 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class OaIdentityTest {
     @Test
     void canParseSampleHarmonyHubOaResponse() throws Exception {
-        String sampleReponse = "<oa xmlns='connect.logitech.com' mime='vnd.logitech.connect/vnd.logitech.pair' errorcode='200' errorstring='OK'>serverIdentity=ea564946-fdce-4bac-1d01-60fcd618e3bd:hubId=106:identity=ea564946-fdce-4bac-1d01-60fcd618e3bd:status=succeeded:protocolVersion={XMPP=\"1.0\", HTTP=\"1.0\", RF=\"1.0\", WEBSOCKET=\"1.0\"}:hubProfiles={Harmony=\"2.0\"}:productId=Pimento:friendlyName=Harmony Hub</oa>";
-        assertThat(OaIdentity.parseHarmonyHubResponse(sampleReponse).asString(), equalTo("ea564946-fdce-4bac-1d01-60fcd618e3bd"));
+        final String sampleResponse = "<oa xmlns='connect.logitech.com' mime='vnd.logitech.connect/vnd.logitech.pair' errorcode='200' errorstring='OK'>serverIdentity=ea564946-fdce-4bac-1d01-60fcd618e3bd:hubId=106:identity=ea564946-fdce-4bac-1d01-60fcd618e3bd:status=succeeded:protocolVersion={XMPP=\"1.0\", HTTP=\"1.0\", RF=\"1.0\", WEBSOCKET=\"1.0\"}:hubProfiles={Harmony=\"2.0\"}:productId=Pimento:friendlyName=Harmony Hub</oa>";
+        assertThat(OaIdentity.parseHarmonyHubResponse(sampleResponse).asString(), equalTo("ea564946-fdce-4bac-1d01-60fcd618e3bd"));
     }
 
     @Test
-    void parsingInvalidXmlThrowsOaIdentityParseException() throws Exception {
-        OaIdentity.OaIdentityParseException exception = assertThrows(OaIdentity.OaIdentityParseException.class, () -> OaIdentity.parseHarmonyHubResponse("barf").asString());
+    void parsingInvalidXmlThrowsOaIdentityParseException() {
+        final OaIdentity.OaIdentityParseException exception = assertThrows(OaIdentity.OaIdentityParseException.class, () -> OaIdentity.parseHarmonyHubResponse("barf").asString());
         assertThat(exception.getMessage(), equalTo("Harmony Hub response is not valid XML"));
     }
 
     @Test
-    void parsingResponseWithNoContentThrowsOaIdentityParseException() throws Exception {
-        OaIdentity.OaIdentityParseException exception = assertThrows(OaIdentity.OaIdentityParseException.class, () -> OaIdentity.parseHarmonyHubResponse("<oa xmlns='connect.logitech.com' mime='vnd.logitech.connect/vnd.logitech.pair' errorcode='200' errorstring='OK'/>").asString());
+    void parsingResponseWithNoContentThrowsOaIdentityParseException() {
+        final OaIdentity.OaIdentityParseException exception = assertThrows(OaIdentity.OaIdentityParseException.class, () -> OaIdentity.parseHarmonyHubResponse("<oa xmlns='connect.logitech.com' mime='vnd.logitech.connect/vnd.logitech.pair' errorcode='200' errorstring='OK'/>").asString());
         assertThat(exception.getMessage(), equalTo("Harmony Hub response does not contain \"identity\" key"));
     }
 
     @Test
-    void parsingResponseWithNoIdentityKeyThrowsOaIdentityParseException() throws Exception {
-        OaIdentity.OaIdentityParseException exception = assertThrows(OaIdentity.OaIdentityParseException.class, () -> OaIdentity.parseHarmonyHubResponse("<oa xmlns='connect.logitech.com' mime='vnd.logitech.connect/vnd.logitech.pair' errorcode='200' errorstring='OK'>serverIdentity=ea564946-fdce-4bac-1d01-60fcd618e3bd:hubId=106:status=succeeded:protocolVersion={XMPP=\"1.0\", HTTP=\"1.0\", RF=\"1.0\", WEBSOCKET=\"1.0\"}:hubProfiles={Harmony=\"2.0\"}:productId=Pimento:friendlyName=Harmony Hub</oa>").asString());
+    void parsingResponseWithNoIdentityKeyThrowsOaIdentityParseException() {
+        final OaIdentity.OaIdentityParseException exception = assertThrows(OaIdentity.OaIdentityParseException.class, () -> OaIdentity.parseHarmonyHubResponse("<oa xmlns='connect.logitech.com' mime='vnd.logitech.connect/vnd.logitech.pair' errorcode='200' errorstring='OK'>serverIdentity=ea564946-fdce-4bac-1d01-60fcd618e3bd:hubId=106:status=succeeded:protocolVersion={XMPP=\"1.0\", HTTP=\"1.0\", RF=\"1.0\", WEBSOCKET=\"1.0\"}:hubProfiles={Harmony=\"2.0\"}:productId=Pimento:friendlyName=Harmony Hub</oa>").asString());
         assertThat(exception.getMessage(), equalTo("Harmony Hub response does not contain \"identity\" key"));
     }
 
